ca29c8bc21d72856837a6f11691652c2260714f6
Jan Andrle <andrle.jan@centrum.cz>
2023-03-01 10:22:46 +0100

    :bug: try/catch for cards

--- utils/listCards.sh
+++ utils/listCards.sh
@@ -1,5 +1,5 @@
 listCards(){
     echo `gh api "projects/columns/$1/cards" -q '. as $arr |
         reduce range(0;length) as $i
-            ([]; . + [ $arr[$i] | { id, note, "number": $i, "issue": (try .content_url | split("/") | .[-1] catch null) } ])'`
+            ([]; . + [ $arr[$i] | { id, note, "number": $i, "issue": (try ( .content_url | split("/") | .[-1] ) catch null) } ])'`
 }
